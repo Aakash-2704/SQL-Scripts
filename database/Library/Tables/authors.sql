4f510dc96a6138ff0ff49a3fb681571a6e59d507
Aakash Kumar Detne <aakash.detne@yahoo.com>
2024-10-19 17:23:10 +0000

    commit

--- database/Library/Tables/authors.sql
+++ database/Library/Tables/authors.sql
@@ -6,6 +6,26 @@ CREATE TABLE [dbo].[authors] (
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
+-- Example 1: Author with a middle name
+INSERT INTO [dbo].[authors] ([first_name], [middle_name], [last_name])
+VALUES ('John', 'Michael', 'Doe');
+
+-- Example 2: Author without a middle name
+INSERT INTO [dbo].[authors] ([first_name], [middle_name], [last_name])
+VALUES ('Jane', NULL, 'Smith');
+
+-- Example 3: Author with a middle name
+INSERT INTO [dbo].[authors] ([first_name], [middle_name], [last_name])
+VALUES ('Emily', 'Grace', 'Johnson');
+
+-- Example 4: Author without a middle name
+INSERT INTO [dbo].[authors] ([first_name], [middle_name], [last_name])
+VALUES ('Robert', NULL, 'Brown');
+
+-- Example 5: Author with a middle name
+INSERT INTO [dbo].[authors] ([first_name], [middle_name], [last_name])
+VALUES ('Alice', 'Marie', 'Williams');
+
 
 GO
 
